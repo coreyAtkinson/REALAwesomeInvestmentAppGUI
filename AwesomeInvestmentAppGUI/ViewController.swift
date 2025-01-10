43c574a0fbc79940ee5a7bad3ca4f7a179b6b0f9
Corey Atkinson <catkinson2025@gmail.com>
2025-01-10 10:51:12 -0600

    Initial Commit!

---
+++ AwesomeInvestmentAppGUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AwesomeInvestmentAppGUI
+//
+//  Created by COREY ATKINSON on 1/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AwesomeInvestmentAppGUITests/AwesomeInvestmentAppGUITests.swift
@@ -0,0 +1,17 @@
+//
+//  AwesomeInvestmentAppGUITests.swift
+//  AwesomeInvestmentAppGUITests
+//
+//  Created by COREY ATKINSON on 1/10/25.
+//
+
+import Testing
+@testable import AwesomeInvestmentAppGUI
+
+struct AwesomeInvestmentAppGUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
